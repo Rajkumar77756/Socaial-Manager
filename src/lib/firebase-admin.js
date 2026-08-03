cac60d49486272e51ea4bd77fb9ec61e2874e1bb
Rajrohit123 <samantdrajkumar@gmail.com>
2026-08-03 12:54:41 +0530

    Fix firebase-admin imports

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,9 +1,11 @@
-import admin from "firebase-admin";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
-if (!admin.apps.length) {
+if (!getApps().length) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
+    initializeApp({
+      credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
@@ -15,7 +17,7 @@ if (!admin.apps.length) {
   }
 }
 
-const adminDb = admin.firestore();
-const adminStorage = admin.storage();
+const adminDb = getFirestore();
+const adminStorage = getStorage();
 
 export { adminDb, adminStorage };
